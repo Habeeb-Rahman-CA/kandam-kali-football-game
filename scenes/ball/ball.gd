c0aa0898b4e96fb1833565aad8ebc849379f1730
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2026-05-01 15:53:28 +0530

    feat: added volley and bicycle kick

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -5,8 +5,6 @@ const BOUNCINESS := 0.8
 const DISTANCE_HIGH_PASS := 130
 enum State {CARRIED, FREEFORM, SHOT}
 
-@export var air_connect_min_height : float
-@export var air_connect_max_height : float
 @export var friction_air : float
 @export var friction_ground : float
 
@@ -57,5 +55,5 @@ func stop() -> void:
 func can_air_interact() -> bool:
 	return current_state != null and current_state.can_air_interact()
 
-func can_air_connect() -> bool:
+func can_air_connect(air_connect_min_height: float, air_connect_max_height: float) -> bool:
 	return height >= air_connect_min_height and height <= air_connect_max_height
